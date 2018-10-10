c35a6feacf58a54220fed9b233e310a281ad46f7
Cara Morelli <caramorelli7@gmail.com>
2018-10-10 20:26:23 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,12 +2,18 @@
 
 class Post 
   
+  attr_accessor :author
+  
   def title=(post_title)
     @title = post_title
   end 
   
   def title
     @title
+  end 
+  
+  def author 
+    
   end 
   
   
